360d3b6f719971e93aba3edc26edc03a6c25b2b1
Abdul Moiz <moiza0799@gmail.com>
2024-11-28 17:37:45 -0600

    Code changes

--- DesignPattern/CreationalPattern/AbstractFactory/Program.cs
+++ DesignPattern/CreationalPattern/AbstractFactory/Program.cs
@@ -1,21 +1,19 @@
-﻿using System.Net.WebSockets;
-
-namespace DesignPattern.CreationalPattern.AbstractFactory
+﻿namespace DesignPattern.CreationalPattern.AbstractFactory
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            var mac = GUIFactory.CreateFactory("MAC");
+        //static void Main(string[] args)
+        //{
+        //    var mac = GUIFactory.CreateFactory("MAC");
 
-            Console.WriteLine(mac.CreateButton().Press());
-            Console.WriteLine(mac.CreateTextBox().ShowText());
+        //    Console.WriteLine(mac.CreateButton().Press());
+        //    Console.WriteLine(mac.CreateTextBox().ShowText());
 
 
-            var windows = GUIFactory.CreateFactory("Windows");
+        //    var windows = GUIFactory.CreateFactory("Windows");
 
-            Console.WriteLine(windows.CreateButton().Press());
-            Console.WriteLine(windows.CreateTextBox().ShowText());
-        }
+        //    Console.WriteLine(windows.CreateButton().Press());
+        //    Console.WriteLine(windows.CreateTextBox().ShowText());
+        //}
     }
 }
